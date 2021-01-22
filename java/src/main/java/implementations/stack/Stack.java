8d80486b62d1438be1b8a48aa9e95ef63519dd00
ArmeetJatyani <armeetjatyani@gmail.com>
2021-01-22 12:23:50 -0800

    Finished Stack! (extends LinkedList)
    Signed-off-by: ArmeetJatyani <armeetjatyani@gmail.com>

---
+++ java/src/main/java/implementations/stack/Stack.java
@@ -0,0 +1,16 @@
+package implementations.stack;
+
+import implementations.linkedlist.LinkedList;
+
+public class Stack extends LinkedList
+{
+    public Stack()
+    {
+        super();
+    }
+
+    public Stack(int value)
+    {
+        super(value);
+    }
+}
---
+++ java/src/main/java/implementations/stack/StackExample.java
@@ -0,0 +1,16 @@
+package implementations.stack;
+
+public class StackExample
+{
+    public static void main(String[] args)
+    {
+        Stack stack = new Stack();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+    }
+}
