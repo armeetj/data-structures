a29b8099598462c7ef92c4867edada5cbb6f5781
ArmeetJatyani <armeetjatyani@gmail.com>
2021-01-22 12:25:52 -0800

    Finished Stack! (extends LinkedList)
    Signed-off-by: ArmeetJatyani <armeetjatyani@gmail.com>

--- java/src/main/java/implementations/stack/Stack.java
+++ java/src/main/java/implementations/stack/Stack.java
@@ -1,6 +1,10 @@
+// @author ArmeetJatyani
+// Stack
+// Stacks extend LinkedLists, which already have a dedicated push and pop method
 package implementations.stack;
 
 import implementations.linkedlist.LinkedList;
+import implementations.linkedlist.LinkedListNode;
 
 public class Stack extends LinkedList
 {
@@ -12,5 +16,27 @@ public class Stack extends LinkedList
     public Stack(int value)
     {
         super(value);
+    }
+
+    // mark linked list specific methods as deprecated
+    @Deprecated
+    @Override
+    public LinkedListNode head()
+    {
+        return null;
+    }
+
+    @Deprecated
+    @Override
+    public LinkedListNode tail()
+    {
+        return null;
+    }
+
+    @Deprecated
+    @Override
+    public void add(int value)
+    {
+        return;
     }
 }
